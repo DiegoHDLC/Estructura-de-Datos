f97e16acc31bc29df17aba13c8ab410097b39167
Diego Herrera De la Calle <49215906+DiegoHDLC@users.noreply.github.com>
2019-05-04 13:35:10 -0400

    Update and rename liblab2.h to liblab2Linux.h

--- liblab2Linux.h
+++ liblab2Linux.h
@@ -3,10 +3,11 @@ typedef struct _lista{
 	int cantidadActual; //cantidad de datos en el arreglo.
 	int cantidadMaxima; //cantidad límite que puede tener este arreglo.
 	int *arreglo; //el arreglo en forma de puntero.
-	int cont;
+
 }ListaNumeros;
 
 //Prototipos de los modulos.
+ListaNumeros *creaListaNumerosordenados(int tamano);
 ListaNumeros *creaListaNumerosVacia(int tamano);
 ListaNumeros *creaListaNumerosLlena(int tamano, int max);
 int eliminaListaNumeros(ListaNumeros *);
@@ -19,9 +20,13 @@ int buscaSecuencialNumeroEnListaNumeros(ListaNumeros *, int);
 int crearNumeroAleatorio(int);
 
 //2.-
+ListaNumeros *insertaEnOrden(ListaNumeros *, int );
+ListaNumeros *InsertarVariosEnOrden(ListaNumeros *,int,int);
 ListaNumeros *ordenamientoLista(ListaNumeros *);
 ListaNumeros *calcularEspacio(ListaNumeros *);
 ListaNumeros *ordenarMenorMayor(ListaNumeros *);
+ListaNumeros *InsertarVariosAlFinal(ListaNumeros *,int,int);
+
 int buscaLugar(ListaNumeros *, int, int*);
 int comparaEnteros(const void *, const void *);
 int busquedaBinaria(ListaNumeros *,int);
@@ -31,21 +36,40 @@ int calculoTiempo(int _inicioSeg, int _finalSeg, int _restaSeg, int _restaNSeg);
 
 //tamano: tamaño del arreglo; - max: el número más grande que puede tener el arreglo.
 //unNumero: Número digitado por usuario para ser buscado.
-
+ListaNumeros *InsertarVariosAlFinal(ListaNumeros *lista3,int unNumero,int max){
+	int i=0;
+	while(i < lista3->cantidadMaxima){
+		unNumero = crearNumeroAleatorio(max);
+		lista3=insertaFinalLista(lista3, unNumero);
+		lista3=ordenamientoLista(lista3);
+		i++;
+	}
+	return lista3;
+}
+ListaNumeros *InsertarVariosEnOrden(ListaNumeros *lista1,int unNumero,int max){
+	int i;
+	for(i = 0; i<lista1->cantidadMaxima && lista1->cantidadActual < lista1->cantidadMaxima; i++){
+		unNumero = crearNumeroAleatorio(max);
+		
+		lista1=insertaEnOrden(lista1,unNumero);
+	}
+	return lista1;
+}
 int guardarEnArchivo(int pasadas, int tamano, int tiempoTotalIns, int tiempoTotalBus, int caso){
 	FILE *fd;
 	if(fd == NULL){
 	//Da un mensaje de error al no existir el archivo archivoAlumnos.txt en este caso.
-	system("read -p 'Error al tratar de agregar datos al archivo\nPresione una tecla para continuar...' var");
-	return 1; //sale de la función;
+		system("read -p 'Error al tratar de agregar datos al archivo\nPresione una tecla para continuar...' var");
+		return 0; //sale de la función;
 	}
 	if(caso == 1){
-	fd = fopen("laboratorio2ED_1.csv","at");
-	
-	}else{
-		if(caso == 2){
+		fd = fopen("laboratorio2ED_1.csv","at");
+	}
+	else if(caso == 2){
 		fd = fopen("laboratorio2ED_2.csv","at");
-		}
+	}
+	else if(caso == 3){
+		fd = fopen("laboratorio2ED_3.csv","at");
 	}
 	fprintf(fd,"%i", pasadas);
 	fprintf(fd,", %i, ", tamano);
@@ -76,10 +100,10 @@ int crearNumeroAleatorio(int max){
 ListaNumeros *creaListaNumerosVacia(int tamano){
 	int i=0;
 
-	ListaNumeros *nuevaLista = malloc(sizeof(ListaNumeros));
+	ListaNumeros *nuevaLista =malloc(sizeof(ListaNumeros));
 	nuevaLista->cantidadActual = 0;
 	nuevaLista->cantidadMaxima = tamano;
-	nuevaLista->cont = 0;
+	
 	nuevaLista->arreglo = malloc(sizeof(int)*tamano);
 	return nuevaLista;
 }
@@ -91,6 +115,18 @@ ListaNumeros *creaListaNumerosLlena(int tamano, int max){
 	srand(time(NULL));
 	for(i = 0; i < nuevaLista->cantidadMaxima; i++){
 		nuevaLista->arreglo[i]= rand()%(max);/*almacena números entre 0 y "max" dentro del arreglo.*/
+	}
+	nuevaLista->cantidadActual = nuevaLista->cantidadMaxima;
+	return nuevaLista;
+}
+
+ListaNumeros *creaListaNumerosordenados(int tamano){
+	int i;
+	ListaNumeros *nuevaLista = creaListaNumerosVacia(tamano);
+	int j=5;
+	for(i = 0; i < nuevaLista->cantidadMaxima; i++){
+		nuevaLista->arreglo[i]= j;
+		j++;
 	}
 	nuevaLista->cantidadActual = nuevaLista->cantidadMaxima;
 	return nuevaLista;
@@ -122,7 +158,7 @@ int buscaSecuencialNumeroEnListaNumeros(ListaNumeros *unaListaNumeros, int unNum
 //imprime los datos almacenados en la lista
 void imprimirLista(ListaNumeros *nuevaLista){
 	int i;
-	for(i=0; i < nuevaLista->cantidadMaxima; i++){
+	for(i=0; i < nuevaLista->cantidadActual; i++){
 		printf("Numero[%i]: %i \n",i+1,nuevaLista->arreglo[i]);
 	}
 	printf("\n");
@@ -140,15 +176,21 @@ ListaNumeros *insertaEnOrden(ListaNumeros *lista, int unNumero){
 
 	//el valor de 'i' se iguala a la cantidad de datos que hay en el arreglo, esto se hace para empezara mover los datos desde el ultimo dato del arreglo.
 	int posicion = 0, i = lista->cantidadActual;
-	
-	buscaLugar(lista,unNumero,&posicion);
-	while(i > posicion){ /*corre los números de izquierda a derecha.*/
-		lista->arreglo[i] = lista->arreglo[i-1];
-		i--;
+	if(lista->cantidadActual==0){
+		lista->arreglo[posicion] = unNumero;
+		lista->cantidadActual++;
 	}
-	//agrega el número entregado por el usuario al arreglo.
-	lista->arreglo[posicion] = unNumero;
-	lista->cantidadActual++;
+	else{
+		buscaLugar(lista,unNumero,&posicion);
+		while(i > posicion){ /*corre los números de izquierda a derecha.*/
+			lista->arreglo[i] = lista->arreglo[i-1];
+			i--;
+		}
+		//agrega el número entregado por el usuario al arreglo.
+		lista->arreglo[posicion] = unNumero;
+		lista->cantidadActual++;
+	}
+	
 	return lista;
 }
 
@@ -178,7 +220,7 @@ ListaNumeros *calcularEspacio(ListaNumeros *listaActual){
 
 //Ordena la lista con el comando qSort
 ListaNumeros *ordenamientoLista(ListaNumeros *lista){
-	qsort(lista->arreglo,lista->cantidadMaxima,sizeof(int), &comparaEnteros);
+	qsort(lista->arreglo,lista->cantidadActual,sizeof(int), &comparaEnteros);
 	return lista;
 }
 
@@ -189,7 +231,7 @@ int comparaEnteros(const void *p, const void *q){
 	int x, y;
 	x = *(int *) p;
 	y = *(int *) q;
-	if(x > y){
+	if(x < y){
 		return -1;
 	}
 	if(x == y ){
@@ -201,9 +243,8 @@ int comparaEnteros(const void *p, const void *q){
 //Busca el número entregado por el usuario mediante busqueda binaria
 int busquedaBinaria(ListaNumeros *lista, int unNumero){
 	int inf,sup,mitad;
-	char band;
-	
-	band = 'F';
+
+
 	inf = 0;
 	sup = lista->cantidadMaxima - 1;
 	mitad = (sup + 1)/2;
@@ -215,8 +256,10 @@ int busquedaBinaria(ListaNumeros *lista, int unNumero){
 			inf = mitad + 1;
 		}
 		mitad = (inf+sup)/2;
+	
 
 	}
+
 	if(lista->arreglo[mitad] == unNumero){
 		return 1;
 	}
